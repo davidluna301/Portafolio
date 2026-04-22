29d0e3cc425dcb1c23484f96e4e7e5b477e70ca0
David Alejandro Luna <david.lunamar@campusucc.edu.co>
2026-04-21 22:27:22 -0500

    erreglar error de direcciones en html

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -9,24 +9,19 @@ import { Contact } from "./pages/Contact";
 import { PersonalLife } from "./pages/PersonalLife";
 import { Music } from "./pages/Music";
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      Component: Layout,
-      children: [
-        { index: true, Component: Home },
-        { path: "about", Component: About },
-        { path: "projects", Component: Projects },
-        { path: "testimonials", Component: Testimonials },
-        { path: "experience", Component: Experience },
-        { path: "personal", Component: PersonalLife },
-        { path: "music", Component: Music },
-        { path: "contact", Component: Contact },
-      ],
-    },
-  ],
+export const router = createBrowserRouter([
   {
-    basename: "/Portafolio",
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true, Component: Home },
+      { path: "about", Component: About },
+      { path: "projects", Component: Projects },
+      { path: "testimonials", Component: Testimonials },
+      { path: "experience", Component: Experience },
+      { path: "personal", Component: PersonalLife },
+      { path: "music", Component: Music },
+      { path: "contact", Component: Contact },
+    ],
   },
-);
+]);
